3c4bae19ee095811844e6d54ea5997d09923bbec
Abashinos <abashinos@gmail.com>
2014-10-02 12:13:18 +0400

    parsers

--- app/src/main/java/org/techteam/bashhappens/fragments/LanguagesList.java
+++ app/src/main/java/org/techteam/bashhappens/fragments/LanguagesList.java
@@ -1,25 +1,55 @@
 package org.techteam.bashhappens.fragments;
 
+
+import org.json.*;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
 public class LanguagesList {
-    private static final String URL = "https://translate.yandex.net/api/v1.5/tr.json/getLangs";
-    private String apiKey;
-    private String ui;
 
+    private Map<String, String> langs = new HashMap<String, String>();
+    private Set<String> dirs = new HashSet<String>();
 
-    public LanguagesList(String apiKey, String ui) {
-        this.apiKey = apiKey;
-        this.ui = ui;
+    private LanguagesList() {
+
     }
 
-    public String getApiKey() {
-        return apiKey;
+    public static LanguagesList fromJsonString(String json) {
+        try {
+            JSONObject obj = new JSONObject(json);
+            LanguagesList languagesList = new LanguagesList();
+
+            JSONArray dirs = obj.getJSONArray("posts");
+            for (int i=0; i<dirs.length(); ++i) {
+                languagesList.dirs.add(dirs.getString(i));
+            }
+
+            JSONObject langs = obj.getJSONObject("langs");
+            Iterator<?> keys = langs.keys();
+
+            while (keys.hasNext()) {
+                String key = (String)keys.next();
+                languagesList.langs.put(key, (String) langs.get(key));
+            }
+
+            return languagesList;
+        }
+        catch (JSONException exc) {
+            exc.printStackTrace();
+            return null;
+        }
     }
 
-    public String getUi() {
-        return ui;
+    public Map<String, String> getLanguages() {
+        return langs;
     }
 
-    public String makeRequest() {
-        return null;
+    public Set<String> getDirections() {
+        return dirs;
     }
 }
+
---
+++ app/src/main/java/org/techteam/bashhappens/fragments/Translation.java
@@ -0,0 +1,46 @@
+package org.techteam.bashhappens.fragments;
+
+import org.json.*;
+
+public class Translation {
+
+    private int code;
+    private String lang;
+    private String text;
+
+    private Translation() {
+
+    }
+
+    public static Translation fromJsonString(String json) {
+        try {
+            JSONObject obj = new JSONObject(json);
+            Translation translation = new Translation();
+            translation.code = obj.getInt("code");
+            translation.lang = obj.getString("lang");
+            StringBuilder text = new StringBuilder();
+            JSONArray arr = obj.getJSONArray("text");
+            for (int i=0; i<arr.length(); ++i) {
+                text.append(arr.getString(i));
+            }
+            translation.text = text.toString();
+            return translation;
+        }
+        catch (JSONException exc) {
+            exc.printStackTrace();
+            return null;
+        }
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getLang() {
+        return lang;
+    }
+
+    public String getText() {
+        return text;
+    }
+}
