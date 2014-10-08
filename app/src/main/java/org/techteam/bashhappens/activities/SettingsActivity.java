18e1c65a19a7556a5fc3b769934e2252f4848094
igorcoding <igorcoding@gmail.com>
2014-10-08 16:33:56 +0400

    Added back button in settings' title

--- app/src/main/java/org/techteam/bashhappens/activities/SettingsActivity.java
+++ app/src/main/java/org/techteam/bashhappens/activities/SettingsActivity.java
@@ -1,8 +1,12 @@
 package org.techteam.bashhappens.activities;
 
+import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.support.v4.app.NavUtils;
+import android.view.MenuItem;
 
 import org.techteam.bashhappens.fragments.SettingsFragment;
 
@@ -11,8 +15,22 @@ public class SettingsActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        ActionBar actionBar = getActionBar();
+        if (actionBar != null)
+            actionBar.setDisplayHomeAsUpEnabled(true);
+
         getFragmentManager().beginTransaction()
                 .replace(android.R.id.content, new SettingsFragment())
                 .commit();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
